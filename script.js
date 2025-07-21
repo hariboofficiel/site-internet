98ec0959e9ff715ae1c2b2d14fd22abc10efa1d1
hariboofficiel <lefoulonmeyer0@gmail.com>
2025-07-21 13:01:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // MODE MAINTENANCE
-const maintenanceMode = true; // Mets false pour afficher le site
+const maintenanceMode = false; // Mets false pour afficher le site
 
 // Menu burger
 const toggle = document.getElementById('menu-toggle');
