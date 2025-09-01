5201259a4515f474812b28441fa562a1d6c9cf95
hariboofficiel <lefoulonmeyer0@gmail.com>
2025-09-01 18:45:18 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,2 +1,7 @@
 // global.js
-const maintenanceMode = false; // ⬅️ Mets à false pour désactiver
+const maintenanceMode = true; // ⬅️ Mets à true pour activer la maintenance
+
+if (maintenanceMode) {
+  // Redirige immédiatement tous les visiteurs vers la page de maintenance
+  window.location.href = "https://noxxworks.netlify.app/maintenance";
+}
