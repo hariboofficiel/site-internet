142dba7363de1445c4d69b91c11d9769cb6e39e2
hariboofficiel <lefoulonmeyer0@gmail.com>
2025-09-01 18:50:18 -0400

    Update global.js

--- global.js
+++ global.js
@@ -1,5 +1,5 @@
 // global.js
-const maintenanceMode = false; // ⬅️ Mets à true pour activer la maintenance
+const maintenanceMode = true; // ⬅️ Mets à true pour activer la maintenance
 
 if (maintenanceMode) {
   // Redirige immédiatement tous les visiteurs vers la page de maintenance
