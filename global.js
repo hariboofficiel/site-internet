4e1c7d5cd75e36edeb8a6ea986db282d4bba9780
Nova Corporation™ <lefoulonmeyer0@gmail.com>
2025-11-25 10:10:00 -0500

    Update global.js

--- global.js
+++ global.js
@@ -1,15 +1,15 @@
 // global.js
-const maintenanceMode = false; // ⬅️ Mets à true pour activer la maintenance 
+const maintenanceMode = true; // ⬅️ Mets à true pour activer la maintenance 
 
 // Redirection automatique
 if (maintenanceMode) {
   // Si on n'est pas déjà sur la page de maintenance, redirige vers elle
   if (!window.location.href.includes("/maintenance")) {
-    window.location.href = "https://novacorporation.online/maintenance";
+    window.location.href = "https://www.novacorporation.online/maintenance";
   }
 } else {
   // Si on est sur la page de maintenance et que la maintenance est désactivée, retourne à l'accueil
   if (window.location.href.includes("/maintenance")) {
-    window.location.href = "https://novacorporation.online/";
+    window.location.href = "https://www.novacorporation.online/";
   }
 }
