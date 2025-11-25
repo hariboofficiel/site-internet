9b91c8b92647e8f3998595a9cbf8ece9860e369d
Nova Corporation™ <lefoulonmeyer0@gmail.com>
2025-11-25 08:38:33 -0500

    Update global.js

--- global.js
+++ global.js
@@ -5,11 +5,11 @@ const maintenanceMode = true; // ⬅️ Mets à true pour activer la maintenance
 if (maintenanceMode) {
   // Si on n'est pas déjà sur la page de maintenance, redirige vers elle
   if (!window.location.href.includes("/maintenance")) {
-    window.location.href = "https://noxxworks.netlify.app/maintenance";
+    window.location.href = "https://site-internet-g1i6.onrender.com/maintenance";
   }
 } else {
   // Si on est sur la page de maintenance et que la maintenance est désactivée, retourne à l'accueil
   if (window.location.href.includes("/maintenance")) {
-    window.location.href = "https://noxxworks.netlify.app/";
+    window.location.href = "https://site-internet-g1i6.onrender.com/";
   }
 }
